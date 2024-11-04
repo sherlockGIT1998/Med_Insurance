fda259cb130f837ed16b7c9be7128597d9c76b5f
carbha01234 <charderajnee20@gmail.com>
2024-11-04 12:31:12 +0530

    all files are added...

---
+++ config.py
@@ -0,0 +1,5 @@
+# import os
+
+
+MODEL_FILE_PATH = r"C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Regression\Medical Insurance\Project_app\linear_model_Medical_Insurance.pkl"
+JSON_FILE_PATH = r"C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Regression\Medical Insurance\Project_app\Project_Med_ins_data.json"
