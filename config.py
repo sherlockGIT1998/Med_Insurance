6dea4f024d330bf7643f2934d3704b52ff3adf1a
sherlockGIT1998 <151735340+sherlockGIT1998@users.noreply.github.com>
2024-11-04 12:49:26 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-# import os
+MODEL_FILE_PATH = r'C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Regression\Medical___Insurance\Project_app\Medical_Insurance.pkl'
 
-
-MODEL_FILE_PATH = r"C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Regression\Medical Insurance\Project_app\linear_model_Medical_Insurance.pkl"
-JSON_FILE_PATH = r"C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Regression\Medical Insurance\Project_app\Project_Med_ins_data.json"
+JSON_FILE_PATH = r'C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Regression\Medical___Insurance\Project_app\Medical_Insurance.json'
--- utils.py
+++ utils.py
@@ -1,61 +1,66 @@
-import pickle
-import json
-import numpy as np
+import pickle 
+import json 
+import numpy as np 
 import pandas as pd
 import warnings
-warnings.filterwarnings("ignore")
-import config
-
+warnings.filterwarnings('ignore')
+import config 
 
 class MedicalInsurance():
-    def __init__(self, age, sex, bmi, children, smoker, region):
-        self.age = age
-        self.sex = sex
+    
+    def __init__(self,age,sex,bmi,children,smoker,region):
+        
+        self.age = age 
+        self.sex = sex 
         self.bmi = bmi
         self.children = children
         self.smoker = smoker
-
-        self.region_col = "region_" + region
-
+        
+        self.region_col = 'region_' + region
+        
     def load_models(self):
-        with open(config.MODEL_FILE_PATH, "rb") as f:
+        
+        with open(config.MODEL_FILE_PATH,'rb') as f:
             self.model = pickle.load(f)
-
-        with open(config.JSON_FILE_PATH, "r") as f:
-            self.save_data  = json.load(f)
-
+            
+        with open(config.JSON_FILE_PATH,'r') as f:
+            self.save_data = json.load(f)
+            
+            self.column_names = self.save_data['column_names']
+            
     def get_predicted_charges(self):
-
-        self.load_models()   
-
-        region_index = list(self.save_data['column_names']).index(self.region_col)
-
+        
+        self.load_models()
+        
+        region_col_index = list(self.save_data['column_names']).index(self.region_col)
+        
         array = np.zeros(len(self.save_data['column_names']))
-
-        array[0] = self.age
-        array[1] = self.save_data['sex_select'][self.sex]
-        array[2] = self.bmi
+        
+        array[0] = self.age 
+        array[1] = self.save_data['sex'][self.sex]
+        array[2] = self.bmi 
         array[3] = self.children
-        array[4] = self.save_data['smoker_select'][self.smoker]
-        array[region_index] = 1
-
-        print("Test Array -->\n", array)
-
+        array[4] = self.save_data['smoker'][self.smoker]
+        array[region_col_index] = 1 
+        
+        print('Array :',array)
+        
         charges = round(self.model.predict([array])[0],2)
-
+        
         return charges
+        
+if __name__ ==  '__main__':
+    
+    age = 19.0
+    sex = 'male'
+    bmi = 27.9
+    children = 0.0  
+    smoker = 'yes'
 
-
-if __name__ == "__main__":
-    age = 30
-    sex = "male"
-    bmi = 24
-    children = 2
-    smoker = "yes"
-    region = "northwest"
-
-    med_ins = MedicalInsurance(age, sex, bmi, children, smoker, region)
+    region = 'northeast'
+    
+    med_ins = MedicalInsurance(age,sex,bmi,children,smoker,region)
+    
     charges = med_ins.get_predicted_charges()
-    print("Predicted Charges:", charges, "/- Rs.")
     
-    
+    print('Predicted Charges :',charges,'/-Rs')
